6f3f96168aae34e980a5d4d55a71b1e3883f3182
VenkeyReddy123 <venkateswarlureddy647@gmail.com>
2023-11-09 21:32:41 +0530

    first commit

---
+++ App1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Venkey1(request):
+    return render(request,'Venkey1.html')
+def Venkey2(request):
+    return render(request,'Venkey2.html')
+def Venkey3(request):
+    return render(request,'Venkey3.html')
+def Venkey4(request):
+    return render(request,'Venkey4.html')
+
+
